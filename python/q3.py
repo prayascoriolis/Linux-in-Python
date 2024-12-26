f24d450cba3557278f3906cd99258011a51013ff
Prayas Kumar <prayas.kumar@coriolis.co.in>
2024-12-26 12:23:57 +0530

    q3

--- python/q3.py
+++ python/q3.py
@@ -1,1 +1,11 @@
 '''3.	Write a program to find the transpose of a matrix.'''
+
+def two_d_matrix(matrix):
+    print(matrix)
+
+def get_transpose():
+    
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [11, 21, 31], [12, 22, 32]]
+    two_d_matrix(matrix)
