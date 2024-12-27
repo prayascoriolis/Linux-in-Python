f775d0be6cc1eaa0379dd1fb74d3c0bc044c13af
Prayas Kumar <prayas.kumar@coriolis.co.in>
2024-12-27 18:12:24 +0530

    regex permutation done

--- python/q16.py
+++ python/q16.py
@@ -1,1 +1,19 @@
 '''16. Develop a script to generate all permutations of a list.'''
+
+def generate_permutations(arr):
+    def permutation_process(current, remaining):
+        if len(remaining) == 0:
+            permutations.append(current)
+        for i in range(len(remaining)):
+            permutation_process(current + [remaining[i]], remaining[:i] + remaining[i+1:])
+
+    permutations = []
+    permutation_process([], arr)
+    return permutations
+
+if __name__ == "__main__":
+    data = [1, 2, 3]
+    result = generate_permutations(data)
+    print("All permutations:")
+    for perm in result:
+        print(perm)
--- python/q27.py
+++ python/q27.py
@@ -1,1 +1,30 @@
 '''27. Write a function to get all matches from a file using a regular expression pattern (All dates, Hyperlinks, Capitalized words, email-id, IP address).'''
+
+import re    
+
+def match_pattern(text, category):
+    pattern_dict = {
+        'email': r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+",
+        'ip': r"[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*",
+        'date': r"[0-9]{4}-[0-9]{2}-[0-9]{2}",
+        'hyperlink': r"https://www\.[a-zA-Z0-9]+\.[a-zA-Z]+/[a-zA-Z0-9]+",
+        'capital': r"[A-Z]+[a-z]+",
+    }
+    match = re.search(pattern_dict[category], text)
+    if match:
+        return match.group()
+    else:
+        return "No match found."
+
+if __name__=="__main__":
+    file_path = "./dir/log_file.txt"
+    with open(file_path, 'r') as file:
+        for line in file:
+            input_str = line.strip()
+            print('Required line:', input_str)
+            print('Email matched:', match_pattern(input_str, 'email'))
+            print('ip matched:', match_pattern(input_str, 'ip'))
+            print('hyperlink matched:', match_pattern(input_str, 'hyperlink'))
+            print('capital matched:', match_pattern(input_str, 'capital'))
+            print('date matched:', match_pattern(input_str, 'date'))
+            print('\n')
