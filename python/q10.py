9f5ba2437a59322de986903795f12e98b1d29fbe
Prayas Kumar <prayas.kumar@coriolis.co.in>
2024-12-27 09:23:55 +0530

    10, 17

--- python/q10.py
+++ python/q10.py
@@ -1,2 +1,25 @@
 '''10. Write a script to find the longest common prefix of a list of strings.'''
 
+def longest_common_prefix(strings):
+    try:
+        # first string as the potential prefix
+        prefix = strings[0]
+        for string in strings[1:]:
+            # Compare the prefix with each string and shorten it if necessary
+            while string[:len(prefix)] != prefix:
+                prefix = prefix[:-1] # up to but not including the last character.
+                if len(prefix) == 0:
+                    return "Doesn't exsist"
+        return prefix
+    except Exception as e:
+        print('Error: ', e)
+
+if __name__ == "__main__":
+    string_lists = [
+        ["prayas", "prajinay", "pratap"],
+        ["ndjscn", "qjdod", "jsj"]
+    ]
+    
+    for strings in string_lists:
+        print(f"Strings: {strings}")
+        print(f"Longest Common Prefix: {longest_common_prefix(strings)}\n")
--- python/q17.py
+++ python/q17.py
@@ -1,1 +1,22 @@
 '''17. Create a function to implement binary search in a sorted list.'''
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+if __name__ == "__main__":
+    sorted_list = [1, 3, 5, 7, 9, 11, 13]
+    target = 7
+    result = binary_search(sorted_list, target)
+    if result != -1:
+        print(f"Element {target} found at index {result}.")
+    else:
+        print(f"Element {target} not found in the list.")
