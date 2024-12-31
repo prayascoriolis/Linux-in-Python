22159eb38a42898cc341e09b0eca96d2f11339b2
Prayas Kumar <prayas.kumar@coriolis.co.in>
2024-12-31 15:22:12 +0530

    subset sum dp

--- python/q31.py
+++ python/q31.py
@@ -1,14 +1,29 @@
 '''31. Create a function to solve the Subset Sum problem using recursion and dynamic programming.'''
 
-def permuteRec(arr, idx, sum1, sum_arr):
-    if idx == len(arr):
-        sum_arr.append(sum1)
-        return
-    permuteRec(arr, idx+1, sum1+arr[idx], sum_arr)
-    permuteRec(arr, idx+1, sum1, sum_arr)
+memo = {}
+def permuteRec(arr, index, current_sum, target):
+
+    # if sum is equal to target
+    if target == current_sum:
+        return True
+    # return false index has crossed arr length or cureent sum is > target
+    if index >= len(arr) or current_sum > target:
+        return False
+
+    # Check if already computed
+    if (index, current_sum) in memo:
+        return memo[(index, current_sum)]
+
+    # include arr[index] in sum
+    include = permuteRec(arr, index+1, current_sum+arr[index], target)
+    # exclude arr[index] in sum
+    exclude = permuteRec(arr, index+1, current_sum, target)
+
+    # store result in memo
+    memo[(index, current_sum)] = include or exclude
+    return memo[(index, current_sum)]
 
 if __name__ == "__main__":
     arr = [1,2,3]
-    sum_arr=[]
-    permuteRec(arr, 0, 0, sum_arr)
-    print(sorted(sum_arr))
+    target = 6
+    print(permuteRec(arr, 0, 0, target))
